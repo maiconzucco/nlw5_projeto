a6abd56dfecaa67f8f41d5173e633e71f40080bb
Maicon Zucco <maicon.zucco@me.com>
2021-05-01 16:02:13 +0100

    Aula 1 - node.js.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nlw5_projeto
 Projeto da Next Level Week #05. 
+
+## Configuração do projeto
+1. yarn add express
+1. yarn add @types/express -D
+1. yarn add typescript -D
+1. yarn tsc --init
+1. yarn add ts-node-dev -D
+
+## Executar a task de execução do projeto
+```typescript
+yarn dev
+```
---
+++ nlw5_nodejs/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+/**
+ * GET: Buscas
+ * POST: Criação
+ * PUT: Alteração
+ * DELETE: Deletar
+ * PATCH: Alterar uma informação específica
+ */
+
+app.get("/", (request, response) => {
+   return response.json({
+       message: "Olá NLW 05!"
+   });
+});
+
+app.post("/users", (request, response) => {
+   return response.json({
+       message: "Usuário salvo com sucess."
+   })
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
